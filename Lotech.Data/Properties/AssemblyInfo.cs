e164cffc26e0cf01d13b4d2b00237e8208df2fe7
cnsicau <cnsicau@163.com>
2018-05-21 22:19:31 +0800

    version 2.0.1

--- Lotech.Data/Properties/AssemblyInfo.cs
+++ Lotech.Data/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.522")]
+[assembly: AssemblyFileVersion("2.0.1.522")]

dfa8e185f97dfd1157f2fc197adde4a07d7102fc
cnsicau <cnsicau@163.com>
2019-07-30 22:40:31 +0800

    fixed:  numeric column values as <string> error

--- Lotech.Data/Queries/ResultMapper.cs
+++ Lotech.Data/Queries/ResultMapper.cs
@@ -49,11 +49,6 @@ namespace Lotech.Data.Queries
         #region Static Members
         readonly static Func<IResultMapper<TValue>> build;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        static readonly Func<IDataRecord, int, TValue> read = CompileReadValue();
-
         static ResultMapper()
         {
             Type mapperType;
@@ -75,73 +70,6 @@ namespace Lotech.Data.Queries
                     Expression.New(mapperType.GetConstructor(Type.EmptyTypes))
                 ).Compile();
         }
-
-        static Func<IDataRecord, int, TValue> CompileReadValue()
-        {
-            var valueType = Nullable.GetUnderlyingType(typeof(TValue)) ?? typeof(TValue);
-            var typeName = Type.GetTypeCode(valueType).ToString();
-
-            #region convert
-
-            var to = typeof(Convert).GetMethod("To" + typeName, new Type[] { typeof(object) });
-
-            var val = Expression.Parameter(typeof(object));
-
-            var convert = Expression.Lambda<Func<object, TValue>>(
-                    Expression.Condition(Expression.ReferenceNotEqual(Expression.Constant(null), val),
-                        to == null ? Expression.Convert(val, typeof(TValue))
-                            : valueType == typeof(TValue) ? Expression.Call(to, val)
-                            : (Expression)Expression.Convert(Expression.Call(to, val), typeof(TValue)),
-                           Expression.Constant(default(TValue), typeof(TValue))
-                        )
-                    , val
-                ).Compile();
-            #endregion
-
-            #region CompileGet
-            var get = typeof(IDataRecord).GetMethod("Get" + typeName, new Type[] { typeof(int) });
-            if (get != null)
-            {
-                var reader = Expression.Parameter(typeof(IDataRecord));
-                var index = Expression.Parameter(typeof(int));
-
-                var read = Expression.Lambda<Func<IDataRecord, int, TValue>>(
-                            valueType == typeof(TValue) ? Expression.Call(reader, get, index)
-                                : (Expression)Expression.Convert(
-                                    Expression.Call(reader, get, index), typeof(TValue))
-                    , reader, index
-                ).Compile();
-
-                if (valueType != typeof(TValue)) // nullable
-                {
-                    return (record, i) =>
-                    {
-                        if (record.IsDBNull(i)) { return default(TValue); }
-                        try { return read(record, i); }
-                        catch { return convert(record.GetValue(i)); }
-                    };
-                }
-
-                return (record, i) =>
-                {
-                    try { return read(record, i); }
-                    catch { return convert(record.GetValue(i)); }
-                };
-            }
-            else
-            {
-                return (record, i) => convert(record.GetValue(i));
-            }
-            #endregion
-        }
-
-        /// <summary>
-        /// 读取值
-        /// </summary>
-        /// <param name="record"></param>
-        /// <param name="i"></param>
-        /// <returns></returns>
-        protected static TValue ReadRecordValue(IDataRecord record, int i) { return read(record, i); }
 
 
         /// <summary>
--- Lotech.Data/Queries/SimpleResultMapper~1.cs
+++ Lotech.Data/Queries/SimpleResultMapper~1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Linq.Expressions;
 
 namespace Lotech.Data.Queries
 {
@@ -8,6 +10,46 @@ namespace Lotech.Data.Queries
     /// <typeparam name="T"></typeparam>
     class SimpleResultMapper<T> : ResultMapper<T>
     {
+        delegate T MapDelegate(IDataRecord record, ref object value);
+
+        static readonly MapDelegate map;
+
+        static SimpleResultMapper()
+        {
+            var record = Expression.Parameter(typeof(IDataRecord), "record");
+            var value = Expression.Parameter(typeof(object).MakeByRefType(), "value");
+
+            var resultType = typeof(T);
+            var valueType = Nullable.GetUnderlyingType(resultType) ?? resultType;
+
+            var assign = Expression.Assign(value, Expression.Call(record
+                     , typeof(IDataRecord).GetMethod(nameof(IDataRecord.GetValue))
+                     , Expression.Constant(0)
+                 ));
+
+            var to = typeof(Convert).GetMethod("To" + valueType.Name, new[] { typeof(object) });
+
+            var isDBNullExpression = Expression.Call(
+                        record, typeof(IDataRecord).GetMethod(nameof(IDataRecord.IsDBNull)), Expression.Constant(0));
+            var valueExpression = to != null ? (Expression)Expression.Call(to, value)
+                : !valueType.IsValueType ? Expression.Convert(value, valueType)
+                : Expression.ConvertChecked(Expression.Call(
+                            typeof(Convert).GetMethod(nameof(Convert.ChangeType), new[] { typeof(object), typeof(Type) }),
+                            value,
+                            Expression.Constant(valueType)), valueType);
+
+            var returnExpression = Expression.Condition(
+                        isDBNullExpression,
+                        Expression.Constant(default(T), resultType),
+                        resultType == valueType ? valueExpression : Expression.ConvertChecked(valueExpression, resultType)
+                    );
+
+            map = Expression.Lambda<MapDelegate>(Expression.Block(
+                    assign,
+                    returnExpression
+                ), record, value).Compile();
+        }
+
         /// <summary>
         /// 映射下一项
         /// </summary>
@@ -17,14 +59,14 @@ namespace Lotech.Data.Queries
         {
             if (reader.Read())
             {
+                object value = null;
                 try
                 {
-                    result = ReadRecordValue(reader, 0);
+                    result = map(reader, ref value);
                     return true;
                 }
                 catch (Exception e)
                 {
-                    var value = reader.GetValue(0);
                     throw new InvalidCastException($"列{reader.GetName(0)}的值“{value}”({value?.GetType()})无法转换为{typeof(T)}.", e);
                 }
             }
